1b98d08b22469008e2404f5f5bf22fa5fcf3ec63
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-13 19:21:26 +0100

    update

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,43 +1,39 @@
-const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers[3]; 
-console.log("1.",fourthTeacher)
+const fourthTeacher = teachers[3];
+console.log("1.", fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers.splice(4,1,'Patrick')
-console.log("2.",teachers)
+teachers.splice(4, 1, "Patrick");
+console.log("2.", teachers);
+
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
 //variante
 //const lastTeacher= teachers.splice(teachers.length-1,1)[0]
-console.log(`3. lastTeacher: ${lastTeacher}, teachers:`, teachers)
+console.log(`3. lastTeacher: ${lastTeacher}, teachers:`, teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
+console.log(`4. firstTeacher: ${firstTeacher}, teachers:`, teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-teachers.push('Vanessa');
+teachers.push("Vanessa");
+console.log("5.", teachers);
+
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-teachers.unshift('Sarah');
+teachers.unshift("Sarah");
+console.log("6.", teachers);
+
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
 const lewisIndex = teachers.indexOf("Lewis");
-console.log(lewisIndex)
+console.log("7.", lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 
 // e salva il risultato nella variabile isTeachersEmpty
 const isTeachersEmpty = teachers.length === 0;
-console.log(isTeachersEmpty)
-
-
+console.log("8.", isTeachersEmpty);
